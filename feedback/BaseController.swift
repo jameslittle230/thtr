b30c2243b34fc2a86b5a24b6628f820eae1cb613
James Little <littleguy23@gmail.com>
2018-08-30 17:12:13 -0400

    App runs and compiles without main storyboard

--- feedback/BaseController.swift
+++ feedback/BaseController.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
