01b03689993804173c3fb7b945b90ec22c6743dd
James Little <littleguy23@gmail.com>
2018-09-04 14:28:45 -0400

    Add labels to feedback sliders

--- feedback/FeedbackViewController.swift
+++ feedback/FeedbackViewController.swift
@@ -70,26 +70,57 @@ class FeedbackItemViewModel {
 
 class FeedbackSliderTableViewCell: UITableViewCell {
     let slider = UISlider()
+    let typeLabel = UILabel()
 
-    var model: FeedbackItemViewModel?
+    var model: FeedbackItemViewModel? {
+        didSet {
+            guard let model = model else {
+                return
+            }
+
+            let label = { () -> String in
+                switch model.type {
+                case .bodyDistortion:
+                    return "Bodies"
+                case .spaceDistortion:
+                    return "Space"
+                case .timeDistortion:
+                    return "Time"
+                }
+            }()
+
+            typeLabel.text = label
+        }
+    }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        // Slider setup
         contentView.addSubview(slider)
         slider.translatesAutoresizingMaskIntoConstraints = false
 
         slider.minimumValue = 0
-        slider.maximumValue = 7
-        slider.value = 4
+        slider.maximumValue = 1
+        slider.value = 0
         slider.isUserInteractionEnabled = true
         slider.addTarget(self, action: #selector(sliderValueDidChange(sender:)), for: .valueChanged)
 
+        // Title setup
+        contentView.addSubview(typeLabel)
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        // Layout constraints
         NSLayoutConstraint.activate([
-            slider.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            slider.topAnchor.constraint(equalTo: typeLabel.bottomAnchor, constant: 6),
             slider.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentView.layoutMargins.left * 3),
             slider.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -contentView.layoutMargins.right),
-            slider.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+            slider.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12)
+            ])
+
+        NSLayoutConstraint.activate([
+            typeLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            typeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentView.layoutMargins.left * 3)
             ])
     }
 
